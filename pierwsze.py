b5b4ac5e14869ac07e3e93ac9a37c6ccc67cffac
juliababizewska <juliababizewsja@gmail.com>
2021-03-23 16:48:00 +0100

    pierwsze-kalkulator

---
+++ pierwsze.py
@@ -0,0 +1,13 @@
+a = float(input("podaj liczbe "))
+b = float(input("podaj liczbe "))
+c = input("podaj znak dzialania ")
+
+if c=="+":
+    print(a+b)
+if c=="-":
+    print(a-b)
+if c=="*":
+    print(a*b)
+if c=="/":
+    print(a/b)
+
